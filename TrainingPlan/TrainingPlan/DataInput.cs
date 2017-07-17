59b992e5f88d0b78cd71be73e90f02e6fc402dcf
subhamsaraf <ssaraf@tavisca.com>
2017-07-17 11:15:23 +0530

    Add files via upload

---
+++ TrainingPlan/TrainingPlan/DataInput.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPlan
+{
+    class DataInput
+    {
+        DataStoreAndGet dataStoreAndGet;
+        public DataInput()
+            {
+                dataStoreAndGet = new DataStoreAndGet();
+                ScheduleInput();
+            }
+
+        public void ScheduleInput()
+        {
+            Console.WriteLine("To set the Training Schedule, Press any key. :)");
+            Console.WriteLine("\t \t Press 0 to go to main application..");
+            string input = Console.ReadLine();
+            while (input != "0")
+            {
+                Console.WriteLine("Please Provide the trainig plan Seperated by (,) ");
+
+                Console.WriteLine("Task:  \t  Assigned Date:  \t No of Trainers Assigned:  \t Scheduled Completion Date:");
+                string[] entry = Console.ReadLine().Split(',');
+                dataStoreAndGet.SetToDoList(entry[0], entry[1], entry[2], int.Parse(entry[3]));
+
+
+                Console.WriteLine("To enter another Plan press any key to Continue, Else enter 0");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TrainingPlan/TrainingPlan/DataStoreAndGet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPlan
+{
+    struct ToDOList
+    {
+         public string taskAssigned;
+         public string assignedDate, completionDate;
+         public int noOfPeopleAssigned;
+    }
+    class DataStoreAndGet
+    {
+        private List<ToDOList> taskList;
+
+        public List<ToDOList> GetToDoList()
+        {
+            return taskList;
+        }
+        public void SetToDoList(string taskAssigned, string assignedDate, string completionDate, int noOfPeopleAssigned)
+        {
+            ToDOList toDoList = new ToDOList();
+            toDoList.taskAssigned = taskAssigned;
+            toDoList.assignedDate = assignedDate;
+            toDoList.completionDate = completionDate;
+            toDoList.noOfPeopleAssigned = noOfPeopleAssigned;
+            taskList.Add(toDoList);
+        }
+    }
+}
